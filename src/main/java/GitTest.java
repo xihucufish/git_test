f795a73c462089e6832c1df80f371058c8608bb5
xihucufish <148667529+xihucufish@users.noreply.github.com>
2023-10-22 17:07:40 +0800

    Update GitTest.java
    sorry my bad

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -8,8 +8,8 @@ public class GitTest {
         System.out.println("hello?git!");
         System.out.println("hello?git!");
         System.out.println("testing commit and push!!!and SSH");
-        # hellooooooooo me edditting from webpage github!!!!
-        # hellooooooooo me edditting from webpage github!!!!
-        # hellooooooooo me edditting from webpage github!!!!
+        //hellooooooooo me edditting from webpage github!!!!
+        //hellooooooooo me edditting from webpage github!!!!
+        //hellooooooooo me edditting from webpage github!!!!
     }
 }
